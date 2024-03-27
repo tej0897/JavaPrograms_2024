49d75b40fe93e84456df8fb5d49ad71e50ac60f3
tej0897 <tej897@gmail.com>
2024-03-27 13:12:02 +0530

    very basic programs update

---
+++ src/main/java/VeryBasicPrograms.java
@@ -0,0 +1,18 @@
+public class VeryBasicPrograms {
+
+    // Swap two variables
+
+    public static void main(String[] args) {
+        String x = "water";
+        String y = "juice";
+        String temp;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
