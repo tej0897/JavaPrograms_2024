590164018a1c4fb6e3a8f665f95f5bb30db0a3a7
tej0897 <tej897@gmail.com>
2024-04-02 07:50:18 +0530

    JOptionPane commit (GUI program-1)

---
+++ src/main/java/BasicPrograms/GUI_Porgrams/exampleProgram.java
@@ -0,0 +1,17 @@
+package BasicPrograms.GUI_Porgrams;
+
+import javax.swing.*;
+
+public class exampleProgram {
+    public static void main(String[] args) {
+
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "you are "+age+" years old");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "you are "+height+" cm tall");
+    }
+}
